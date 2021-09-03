901f7b40200ad3e6a548d4e673088fdc647fa604
Ricardo Lima Mazzolli <ricarm@gmail.com>
2021-09-02 21:11:18 -0300

    Inclusão do app de HTTPS e exclusão do django_summernote

---
+++ https/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Chave
+
+
+@admin.register(Chave)
+class ChaveAdmin(admin.ModelAdmin):
+    list_display = ['chave', 'data_hora', 'verificada']
