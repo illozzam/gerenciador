782ea5d181eb4fee5fbb7e46e4ddd7b85b57934b
Ricardo Lima Mazzolli <ricarm@gmail.com>
2021-09-14 14:46:43 -0300

    Modificando título da tela de fluxo de caixa e iniciando alguns testes unitários

--- financeiro/tests/contas_a_pagar.py
+++ financeiro/tests/contas_a_pagar.py
@@ -20,3 +20,17 @@ class ContasAPagarTestCase(TestCase):
         )
         resposta = self.cliente.get('/financeiro/contas-a-pagar/')
         self.assertEqual(resposta.status_code, 200)
+
+    # def test_cadastrando_conta_a_pagar(self):
+    #     self.cliente.login(
+    #         username=self.dados_usuario['username'],
+    #         password=self.dados_usuario['password']
+    #     )
+    #     dados_conta = {
+    #         'codigo_barras': '0' * 48,
+    #         'data': '2021-09-10',
+    #         'valor': '100',
+    #         'descricao': 'Teste de conta a pagar',
+    #     }
+    #     resposta = self.cliente.put('/financeiro/contas-a-pagar/', data=dados_conta)
+    #     self.assertEqual(resposta.status_code, 200)
--- principal/tests/factories.py
+++ principal/tests/factories.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from faker import Faker
-from principal.models import Usuario
 
 
 class UsuarioFactory:
@@ -12,9 +11,6 @@ class UsuarioFactory:
         self.username = f'{self.first_name.lower()}.{self.last_name.lower()}'
         self.email = f'{self.username}@yopmail.com'
         self.password = self.fake.password()
-        self.telefone = self.fake.phone_number()
-        self.cargo = 'TESTE'
-        self.facebook = 'TESTE'
 
     def dados(self):
         return {
@@ -23,9 +19,6 @@ class UsuarioFactory:
             'username': self.username,
             'email': self.email,
             'password': self.password,
-            'telefone': self.telefone,
-            'cargo': self.cargo,
-            'facebook': self.facebook,
         }
 
     def cadastra(self):
@@ -34,6 +27,4 @@ class UsuarioFactory:
         usuario_django.last_name = self.last_name
         usuario_django.set_password(self.password)
         usuario_django.save()
-
-        usuario_aplicacao = Usuario.objects.create(usuario=usuario_django, telefone=self.telefone, facebook=self.facebook, cargo=self.cargo)
-        return usuario_aplicacao
+        return usuario_django
