f973b7e38e02566ba2494f7501cc6e7445bb3983
Ricardo Lima Mazzolli <ricarm@gmail.com>
2022-07-21 22:04:16 -0300

    Implementando funcionalidade de categoria e controle de data da importação do extrato bancário, além de dados iniciais

--- financeiro/services/extrato_bancario.py
+++ financeiro/services/extrato_bancario.py
@@ -1,8 +1,10 @@
 import codecs
 import csv
-from typing import List, Dict
 from datetime import datetime
-from financeiro.models import TermoCategoria, FluxoDeCaixa
+from typing import List
+
+from financeiro.models import FluxoDeCaixa, TermoCategoria
+from principal.models import Config
 
 
 class ExtratoBancarioService:
@@ -18,8 +20,22 @@ class ExtratoBancarioService:
         return movimentacoes
 
     @classmethod
+    def __atualiza_ultima_data_extrato_bancario(
+        cls, movimentacoes: List[FluxoDeCaixa], ultima_data: datetime.date
+    ) -> None:
+        if movimentacoes[-1].data > ultima_data:
+            Config.objects.filter(variavel="ultima_data_extrato_bancario").update(
+                valor=movimentacoes[-1].data.isoformat()
+            )
+
+    @classmethod
     def extrair(cls, arquivo) -> List[FluxoDeCaixa]:
         leitor = csv.reader(codecs.iterdecode(arquivo, "utf-8"), delimiter=";")
+        ultima_data = datetime.date(
+            datetime.fromisoformat(
+                Config.objects.get(variavel="ultima_data_extrato_bancario").valor
+            )
+        )
         try:
             movimentacoes = cls.__preenche_categorias(
                 list(
@@ -35,11 +51,17 @@ class ExtratoBancarioService:
                         [
                             transacao
                             for transacao in list(leitor)
-                            if len(transacao) == 4 and transacao[0] != "DATA LANÇAMENTO"
+                            if len(transacao) == 4
+                            and transacao[0] != "DATA LANÇAMENTO"
+                            and datetime.date(
+                                datetime.strptime(transacao[0], "%d/%m/%Y")
+                            )
+                            > ultima_data
                         ],
                     )
                 )
             )
+            cls.__atualiza_ultima_data_extrato_bancario(movimentacoes, ultima_data)
         except Exception as erro:
             raise Exception(erro)
 
