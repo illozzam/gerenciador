eddedbeb464afaa1ea2023fd23ce40428751e8b9
Ricardo Lima Mazzolli <ricarm@gmail.com>
2022-07-31 17:44:10 -0300

    Refatorando inicio.py e removendo entrada do requirements.txt com erro

--- inicio.py
+++ inicio.py
@@ -1,63 +1,73 @@
-from principal.models import Config
+from django.contrib.auth.models import User
+
 from financeiro.models import Categoria, TermoCategoria
+from principal.models import Config
 
 
 class Inicio:
-    @staticmethod
-    def dados_iniciais():
-        print("Criando config...")
-        Config.objects.create(
-            variavel="ultima_data_extrato_bancario", valor="1900-01-01"
+    def __init__(self):
+        configs = [
+            Config.objects.create(
+                variavel="ultima_data_extrato_bancario", valor="1900-01-01"
+            )
+        ]
+
+        categorias = [
+            Categoria.objects.create(nome="Deslocamento"),
+            Categoria.objects.create(nome="Recarga de Celular"),
+            Categoria.objects.create(nome="Delivery"),
+            Categoria.objects.create(nome="Suprimentos"),
+            Categoria.objects.create(nome="Luz"),
+            Categoria.objects.create(nome="Água SAMAE"),
+            Categoria.objects.create(nome="Apartamento"),
+            Categoria.objects.create(nome="Salários"),
+            Categoria.objects.create(nome="Internet"),
+            Categoria.objects.create(nome="MEI"),
+            Categoria.objects.create(nome="ClinCard"),
+            Categoria.objects.create(nome="Feira"),
+            Categoria.objects.create(nome="Reserva"),
+        ]
+
+        termos_categorias = [
+            TermoCategoria.objects.create(termo="*UBER", categoria=categorias[0]),
+            TermoCategoria.objects.create(termo="uber", categoria=categorias[0]),
+            TermoCategoria.objects.create(
+                termo="RECARGA CELULAR", categoria=categorias[1]
+            ),
+            TermoCategoria.objects.create(termo="19468242", categoria=categorias[2]),
+            TermoCategoria.objects.create(
+                termo="SDB COM DE ALIMENTOS", categoria=categorias[3]
+            ),
+            TermoCategoria.objects.create(termo="Luz", categoria=categorias[4]),
+            TermoCategoria.objects.create(termo="SAMAE", categoria=categorias[5]),
+            TermoCategoria.objects.create(termo="00360305", categoria=categorias[6]),
+            TermoCategoria.objects.create(termo="37880206", categoria=categorias[7]),
+            TermoCategoria.objects.create(termo="Internet", categoria=categorias[8]),
+            TermoCategoria.objects.create(termo="INTERNET", categoria=categorias[8]),
+            TermoCategoria.objects.create(
+                termo="MEI Julyanna", categoria=categorias[9]
+            ),
+            TermoCategoria.objects.create(
+                termo="MEI JULYANNA", categoria=categorias[9]
+            ),
+            TermoCategoria.objects.create(termo="MEI", categoria=categorias[9]),
+            TermoCategoria.objects.create(termo="Clincard", categoria=categorias[10]),
+            TermoCategoria.objects.create(termo="ClinCard", categoria=categorias[10]),
+            TermoCategoria.objects.create(termo="CLINCARD", categoria=categorias[10]),
+            TermoCategoria.objects.create(termo="19540550", categoria=categorias[10]),
+            TermoCategoria.objects.create(
+                termo="DIRETO DOS VERDES VALE", categoria=categorias[11]
+            ),
+            TermoCategoria.objects.create(termo="18236120", categoria=categorias[12]),
+        ]
+
+        User.objects.create_superuser(
+            username="mazzolli",
+            password="p4ssw0rd",
+            email="ricarm@gmail.com",
+            first_name="Ricardo",
+            last_name="Mazzolli",
         )
 
-        print("Criando categorias...")
-        cat = Categoria.objects.create(nome="Deslocamento")
-        TermoCategoria.objects.create(termo="*UBER", categoria=cat)
-        TermoCategoria.objects.create(termo="uber", categoria=cat)
 
-        cat = Categoria.objects.create(nome="Recarga de Celular")
-        TermoCategoria.objects.create(termo="RECARGA CELULAR", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Delivery")
-        TermoCategoria.objects.create(termo="19468242", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Suprimentos")
-        TermoCategoria.objects.create(
-            termo="SDB COM DE ALIMENTOS PALHOCA", categoria=cat
-        )
-
-        cat = Categoria.objects.create(nome="Luz")
-        TermoCategoria.objects.create(termo="Luz", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Água SAMAE")
-        TermoCategoria.objects.create(termo="SAMAE", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Apartamento")
-        TermoCategoria.objects.create(termo="00360305", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Salários")
-        TermoCategoria.objects.create(termo="37880206", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Internet")
-        TermoCategoria.objects.create(termo="Internet", categoria=cat)
-        TermoCategoria.objects.create(termo="INTERNET", categoria=cat)
-
-        cat = Categoria.objects.create(nome="MEI")
-        TermoCategoria.objects.create(termo="MEI Julyanna", categoria=cat)
-        TermoCategoria.objects.create(termo="MEI JULYANNA", categoria=cat)
-        TermoCategoria.objects.create(termo="MEI", categoria=cat)
-
-        cat = Categoria.objects.create(nome="ClinCard")
-        TermoCategoria.objects.create(termo="Clincard", categoria=cat)
-        TermoCategoria.objects.create(termo="CLINCARD", categoria=cat)
-        TermoCategoria.objects.create(termo="ClinCard", categoria=cat)
-        TermoCategoria.objects.create(termo="19540550", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Feira")
-        TermoCategoria.objects.create(termo="DIRETO DOS VERDES VALE", categoria=cat)
-
-        cat = Categoria.objects.create(nome="Reserva")
-        TermoCategoria.objects.create(termo="18236120", categoria=cat)
-
-
-Inicio.dados_iniciais()
+Inicio()
